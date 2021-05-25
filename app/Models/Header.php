3b17f456609f4bf4b3d4cb7d3569b9b557474376
Yididya_G_Abraha <yidu95@gmail.com>
2021-05-25 10:54:50 +0300

    models --a header

---
+++ app/Models/Header.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Header extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'image_url'];
+
+    public function UpdateHeader($title, $slug, $image_url)
+    {
+        $header = $this::first();
+        $header->title = $title;
+        $header->slug = $slug;
+        $header->image_url = $image_url;
+        $header->save();
+    }
+
+    public function GetHeader()
+    {
+        return $this::first();
+    }
+}
